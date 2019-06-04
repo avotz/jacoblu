c5f21e8791c62fe3f6a90b2f942d5aa8b51c708d
Alonso <alonso@avotz.com>
2019-06-04 17:15:24 -0600

    se cambio link footer a whatapp

--- wp-content/themes/jacoblu/footer.php
+++ wp-content/themes/jacoblu/footer.php
@@ -11,52 +11,52 @@
 
 ?>
 <?php /*if ( !is_home() && ! is_front_page() ) :*/ ?>
-	
+
 
 <!-- <section class="section-contact">
 		<div class="inner">
 			<a href="#" class="btn">Discover Us</a>
 		</div>
 	</section> -->
-	<footer class="footer">
-		<div class="inner">
-			<div class="footer-items">	
-				<div class="footer-social">
-					<h3>Follow Us</h3>
-					<a href="#" class="footer-social-item"><i class="icon-facebook"></i></a>
-					<a href="#" class="footer-social-item"><i class="icon-twitter"></i></a>
-					<a href="#" class="footer-social-item"><i class="fa fa-google-plus"></i></a>
-					<a href="#" class="footer-social-item"><i class="fa fa-instagram"></i></a>
-					<a href="#" class="footer-social-item"><i class="fa fa-tripadvisor"></i></a>
-					<address>
-					<i class="icon-phone"></i> (506) 2643-4372<br>
+<footer class="footer">
+	<div class="inner">
+		<div class="footer-items">
+			<div class="footer-social">
+				<h3>Follow Us</h3>
+				<a href="#" class="footer-social-item"><i class="icon-facebook"></i></a>
+				<a href="#" class="footer-social-item"><i class="icon-twitter"></i></a>
+				<a href="#" class="footer-social-item"><i class="fa fa-google-plus"></i></a>
+				<a href="#" class="footer-social-item"><i class="fa fa-instagram"></i></a>
+				<a href="#" class="footer-social-item"><i class="fa fa-tripadvisor"></i></a>
+				<address>
+					<a href="https://wa.me/50684442121"> <i class="icon-phone"></i>8444-2121</a><br>
 					<i class="fa fa-envelope-o"></i> info@jacoblu.com
-					</address>
-					
-					
-				</div>
-				<div class="footer-address">
-					<h3>Location</h3>
-					<address>
-						
-						Del Parque Central de Jaco, 100m N, 100m O.<br>
-						Jaco, Costa Rica
-					</address>
-					<a href="waze://?q=Jacó+Blu+Beach+Club&amp;navigate=yes"  target="_blank" class="btn-app"><i class="fa fa-map-marker"></i> <strong>Abrir en Waze</strong></a>
+				</address>
 
-              		<a href="http://maps.google.com/?saddr=Current+Location&daddr=Jacó+Blu+Beach+Club" target="_blank" class="btn-app"><i class="fa fa-map-marker"></i> <strong>Abir en Google Maps</strong></a>
-				</div>
-				<div class="footer-copyright">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="footer-logo"><img src="<?php echo get_template_directory_uri();  ?>/img/logo.png" alt="Jaco blu" /></a>
-					<span>Copyright &copy; 2017. Avotz</span>
-				</div>
-			
+
 			</div>
-		</div>
+			<div class="footer-address">
+				<h3>Location</h3>
+				<address>
 
-	</footer>
+					Del Parque Central de Jaco, 100m N, 100m O.<br>
+					Jaco, Costa Rica
+				</address>
+				<a href="waze://?q=Jacó+Blu+Beach+Club&amp;navigate=yes" target="_blank" class="btn-app"><i class="fa fa-map-marker"></i> <strong>Abrir en Waze</strong></a>
+
+				<a href="http://maps.google.com/?saddr=Current+Location&daddr=Jacó+Blu+Beach+Club" target="_blank" class="btn-app"><i class="fa fa-map-marker"></i> <strong>Abir en Google Maps</strong></a>
+			</div>
+			<div class="footer-copyright">
+				<a href="<?php echo esc_url(home_url('/')); ?>" class="footer-logo"><img src="<?php echo get_template_directory_uri();  ?>/img/logo.png" alt="Jaco blu" /></a>
+				<span>Copyright &copy; 2017. Avotz</span>
+			</div>
+
+		</div>
+	</div>
+
+</footer>
 <?php /*endif */ ?>
-	<!-- <div class="float-form">
+<!-- <div class="float-form">
 		<div class="float-form-header">
 			<div class="float-form-header-bg"></div>
 			 <div class="title">
@@ -91,9 +91,10 @@
 		
 	</div> -->
 
-	
+
 
 <?php wp_footer(); ?>
 
 </body>
+
 </html>
